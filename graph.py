41ab98e1c7090fdb184987b3c0a3d0eab1266099
devprogst <dev.progst@gmail.com>
2023-01-30 17:46:16 +0300

    upgradedGraph

--- graph.py
+++ graph.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import glob
 
 graphs = []
-files = [ 'localhost_20000_log.log', 'localhost_20001_log.log', 'localhost_20002_log.log', 'localhost_20003_log.log', 'localhost_20004_log.log', 'localhost_20005_log.log' ]
+files = glob.glob('*_log.log')
 clrs = [ 'b', 'g', 'r', 'c', 'm', 'k', 'y' ]
 for f in files:
 	with open(f, "r") as file:
@@ -17,6 +18,6 @@ plt.xlabel('Итерация (N)', color='gray')
 plt.ylabel('Значение (%)',color='gray')
 
 for gid, g in enumerate(graphs):
-	plt.plot(range(len(g)), g, clrs[gid] + '-')
+	plt.plot(range(len(g)), g, clrs[gid % len(clrs)] + '-')
 
 plt.show()

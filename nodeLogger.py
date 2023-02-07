a516aa27cfd1dffbfe796147510cc69a30f96169
devprogst <dev.progst@gmail.com>
2023-02-07 15:58:58 +0300

    clean

--- nodeLogger.py
+++
@@ -1,8 +0,0 @@
-
-def cleanLog(saveFile):
-  with open(saveFile + ".log", "w"): pass
-
-def writeLog(saveFile, block, loss, acc):
-  with open(saveFile + ".log", "a") as sf:
-    data = map(str, [ block, loss, acc ])
-    sf.write(",".join(data) + "\n")
--- starter.bat
+++
@@ -1,6 +0,0 @@
-start cmd /k python main.py 0 localhost:20000 localhost:20001 localhost:20002
-start cmd /k python main.py 1 localhost:20001 localhost:20002 localhost:20003
-start cmd /k python main.py 2 localhost:20002 localhost:20003 localhost:20004
-start cmd /k python main.py 3 localhost:20003 localhost:20004 localhost:20005
-start cmd /k python main.py 4 localhost:20004 localhost:20005 localhost:20000
-start cmd /k python main.py 5 localhost:20005 localhost:20000 localhost:20001
